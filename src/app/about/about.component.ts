f665709581bbffa0b15736217c91ed818e368639
Margo Yermachenko <margoermachenko@gmail.com>
2024-04-09 22:24:22 +0200

    How works .pipe( map()) for http$. How accept data from the server

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { count } from 'console';
 import { response } from 'express';
 import { Observable, Observer, fromEvent, interval, timer } from 'rxjs';
+import { createHttpObservable } from '../common/util';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'about',
@@ -16,21 +18,14 @@ export class AboutComponent implements OnInit {
 
   ngOnInit() { 
 
-    const http$ = new Observable<void>((observer) => {
-      fetch('/api/courses') 
-        .then(response => {
-          return response.json();
-        })
-        .then(body => {
-          observer.next(body);
-          observer.complete();
-        })
-        .catch(err => {
-          observer.error(err);
-        })
-    })
+    const http$ = createHttpObservable('/api/courses');
 
-    http$.subscribe(
+    const courses$ = http$
+      .pipe(
+        map(result => Object.values(result["payload"]))
+      )
+
+    courses$.subscribe(
       courses => console.log(courses),
       () => {}, //noop = no operation
       () => console.log("Completed!")
@@ -38,7 +33,5 @@ export class AboutComponent implements OnInit {
     
   }
 
- 
-
-
+  
 }
