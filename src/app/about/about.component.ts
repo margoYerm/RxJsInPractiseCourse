b567a441e9a4a21b08aa75f8b26e31ef1b1bca0a
Margo Yermachenko <margoermachenko@gmail.com>
2024-04-11 17:56:48 +0200

    RxJs concat() operator - result of concatenation of few streams

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { count } from 'console';
 import { response } from 'express';
-import { Observable, Observer, fromEvent, interval, timer } from 'rxjs';
+import { Observable, Observer, concat, fromEvent, interval, of, timer } from 'rxjs';
 import { createHttpObservable } from '../common/util';
 import { map } from 'rxjs/operators';
 
@@ -18,7 +18,14 @@ export class AboutComponent implements OnInit {
 
   ngOnInit() { 
 
+    const source1$ = of(1, 2, 3);
+    const source2$ = of(4, 5, 6);
+    const source3$ = of(7, 8, 9);
     
+    const result$ = concat(source1$, source2$, source3$);
+    //result$.subscribe(val => console.log(val)); 
+    //the same result with different syntax 
+    result$.subscribe(console.log); 
     
   }
 
