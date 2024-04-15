4a79a41e9be6e0011fce18ade081c14e5a7a359a
Margo Yermachenko <margoermachenko@gmail.com>
2024-04-16 00:17:50 +0200

    Added data to Course component: List of Course lessons. Resolved error ts 2322 (Type Observable<void> is not assignable to type Observable<Course>

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -15,6 +15,7 @@ import {
 } from 'rxjs/operators';
 import {merge, fromEvent, Observable, concat} from 'rxjs';
 import {Lesson} from '../model/lesson';
+import { createHttpObservable } from '../common/util';
 
 
 @Component({
@@ -22,36 +23,26 @@ import {Lesson} from '../model/lesson';
     templateUrl: './course.component.html',
     styleUrls: ['./course.component.css']
 })
-export class CourseComponent implements OnInit, AfterViewInit {
-
+export class CourseComponent implements OnInit {
 
     course$: Observable<Course>;
     lessons$: Observable<Lesson[]>;
 
-
     @ViewChild('searchInput', { static: true }) input: ElementRef;
 
-    constructor(private route: ActivatedRoute) {
-
-
-    }
+    constructor(private route: ActivatedRoute) {}
 
     ngOnInit() {
 
         const courseId = this.route.snapshot.params['id'];
+        this.course$ = createHttpObservable(`/api/courses/${courseId}`) as 
+            Observable<unknown> as Observable<Course>
 
-
+        this.lessons$ = 
+            createHttpObservable(`/api/lessons?courseId=${courseId}&pageSize=100`)
+                .pipe(
+                    map(res => res["payload"])
+                )
 
     }
-
-    ngAfterViewInit() {
-
-
-
-
-    }
-
-
-
-
 }
