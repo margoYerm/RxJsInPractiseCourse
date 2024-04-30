8079de1d62dd6640a64b6a87969b3ba6fd384c5f
Margo Yermachenko <margoermachenko@gmail.com>
2024-04-30 16:29:06 +0200

    Subject simple explanation

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { count } from 'console';
 import { response } from 'express';
-import { Observable, Observer, concat, fromEvent, interval, merge, of, timer } from 'rxjs';
+import { Observable, Observer, Subject, concat, fromEvent, interval, merge, of, timer } from 'rxjs';
 import { createHttpObservable } from '../common/util';
 import { map } from 'rxjs/operators';
 
@@ -18,34 +18,19 @@ export class AboutComponent implements OnInit {
 
   ngOnInit() { 
 
-    const source1$ = of(1, 2, 3);
-    const source2$ = of(4, 5, 6);
-    const source3$ = of(7, 8, 9);
+    const subject = new Subject();
+    const series1$ = subject.asObservable();
+    series1$.subscribe(console.log);
+
+    //from(document, 'keyup');
+
+    subject.next(1);
+    subject.next(2);
+    subject.next(3);
+    subject.complete()
+
     
-    const result$ = concat(source1$, source2$, source3$);
-    //result$.subscribe(val => console.log(val)); 
-    //the same result with different syntax 
-    //result$.subscribe(console.log); 
 
-    const interval1$ = interval(1000);
-    const interval2$ = interval1$.pipe(
-      map(val => val * 10)
-    );
-    const resultInterval$ = merge(interval1$, interval2$);
-    //resultInterval$.subscribe(console.log); //0 0 1 10 2 20....
-
-    //unsubscription from observable 
-    const intervalUns$ = interval(1000);
-    //const sub = intervalUns$.subscribe(console.log);
-    //setTimeout(() => {sub.unsubscribe()}, 5000);
-
-    //unsubscription from HTTP request 
-    const httpReq$ = createHttpObservable('/api/courses');
-    //const unsubHttpReg = httpReq$.subscribe(console.log);
-    //setTimeout(() => {unsubHttpReg.unsubscribe()}, 0)
   }
-
-  
-
-  
 }
+
