3ed20a1dd5070ab006452e1d006b1257b1ac3f0a
Margo Yermachenko <margoermachenko@gmail.com>
2024-04-30 18:21:02 +0200

    How works BehaviorSubject with early and late subscriptions

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { count } from 'console';
 import { response } from 'express';
-import { Observable, Observer, Subject, concat, fromEvent, interval, merge, of, timer } from 'rxjs';
+import { BehaviorSubject, Observable, Observer, Subject, concat, fromEvent, interval, merge, of, timer } from 'rxjs';
 import { createHttpObservable } from '../common/util';
 import { map } from 'rxjs/operators';
 
@@ -18,18 +18,33 @@ export class AboutComponent implements OnInit {
 
   ngOnInit() { 
 
-    const subject = new Subject();
+    /*const subject = new Subject();
     const series1$ = subject.asObservable();
-    series1$.subscribe(console.log);
-
-    //from(document, 'keyup');
+    series1$.subscribe(val => console.log("early subscription: " + val));
 
     subject.next(1);
     subject.next(2);
     subject.next(3);
-    subject.complete()
+    //subject.complete()
 
-    
+    setTimeout(() => {
+      series1$.subscribe(val => console.log("late subscription: " + val));
+      subject.next(4);
+    }, 3000)*/
+
+    const subject = new BehaviorSubject(0);
+    const series1$ = subject.asObservable();
+    series1$.subscribe(val => console.log("early subscription: " + val));
+
+    subject.next(1);
+    subject.next(2);
+    subject.next(3);
+    //subject.complete()
+
+    setTimeout(() => {
+      series1$.subscribe(val => console.log("late subscription: " + val));
+      subject.next(4);
+    }, 3000)
 
   }
 }
