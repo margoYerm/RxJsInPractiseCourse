8d22764b4e0f3ff1cd89c3742a8b441347feb3ad
Margo Yermachenko <margoermachenko@gmail.com>
2024-05-02 00:27:17 +0200

    Refactored CoursesComponent for using store for course$ observable. Added selector for this - selectCourseById()

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -17,6 +17,7 @@ import {merge, fromEvent, Observable, concat, interval, forkJoin} from 'rxjs';
 import {Lesson} from '../model/lesson';
 import { createHttpObservable } from '../common/util';
 import { RxJsLoggingLevel, debug, setRxJsLoggingLevel } from '../common/debug';
+import { Store } from '../common/store.service';
 
 
 @Component({
@@ -25,18 +26,22 @@ import { RxJsLoggingLevel, debug, setRxJsLoggingLevel } from '../common/debug';
     styleUrls: ['./course.component.css']
 })
 export class CourseComponent implements OnInit, AfterViewInit{
-    courseId: string;
+    courseId: number;
     course$: Observable<Course>;
     lessons$: Observable<Lesson []>
    
     @ViewChild('searchInput', { static: true }) input: ElementRef;
 
-    constructor(private route: ActivatedRoute) {}
+    constructor(
+        private route: ActivatedRoute,
+        private store: Store
+        ) {}
 
     ngOnInit() {
         this.courseId = this.route.snapshot.params['id'];
-        this.course$ = (createHttpObservable(`/api/courses/${this.courseId}`) as 
-            Observable<unknown> as Observable<Course>)               
+        /*this.course$ = (createHttpObservable(`/api/courses/${this.courseId}`) as 
+            Observable<unknown> as Observable<Course>)  */
+        this.course$ = this.store.selectCourseById(this.courseId)             
     }
 
     ngAfterViewInit() {            
